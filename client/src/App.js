eff2e647450d07ebc4a84c1fea66c4fc914bf0ed
luffyguy <harshtanwar25@gmail.com>
2021-10-16 23:27:05 +0530

    added room route

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import { Switch, Route } from "react-router-dom";
 
 //components
 import Home from "./components/Home/Home";
-
 import CloudinaryUpload from "./components/cloudinary/upload";
 import CloudinaryImages from "./components/cloudinary/images";
 import Compiler from "./components/compiler/Compiler";
+import Room from "./components/views/room";
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
         <Route path="/cloudinary/upload" component={CloudinaryUpload} />
         <Route path="/cloudinary/images" component={CloudinaryImages} />
         <Route path="/compiler" component={Compiler} />
+        <Route path="/room" component={Room} />
       </Switch>
     </div>
   );
