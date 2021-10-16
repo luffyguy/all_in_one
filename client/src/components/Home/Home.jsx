dfc764dcad777ad655a76ab707498e8b90c8f540
luffyguy <harshtanwar25@gmail.com>
2021-10-16 23:28:53 +0530

    make room on home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
+  const [id, setId] = useState("");
 
-export default Home
+  useEffect(() => {
+    setId(uuidv4());
+  }, []);
+
+  return (
+    <>
+      <h1>HOME</h1>
+      <Link to={`/room/${id}`}>
+        <button>Create a Room</button>
+      </Link>
+    </>
+  );
+};
+
+export default Home;
